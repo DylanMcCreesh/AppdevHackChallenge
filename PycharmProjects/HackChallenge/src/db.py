1e68a4ed1ab7cd720820092af85b858e8040e86d
Dylan McCreesh <dpm256@cornell.edu>
2021-12-03 01:11:14 -0500

    update db.py and app.py

--- PycharmProjects/HackChallenge/src/db.py
+++ PycharmProjects/HackChallenge/src/db.py
@@ -12,7 +12,7 @@ association_table = db.Table(
 )
 
 class Fan(db.Model):
-    __tablename__ = "fans"
+    __tablename__ = "fan"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)

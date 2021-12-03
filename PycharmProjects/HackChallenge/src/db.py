00ba1fc0d6d2abc637998a6a02e420eadd32065c
dylan <dylanmccreesh@gmail.com>
2021-12-03 00:55:53 -0500

    hi

--- PycharmProjects/HackChallenge/src/db.py
+++ PycharmProjects/HackChallenge/src/db.py
@@ -12,7 +12,7 @@ association_table = db.Table(
 )
 
 class Fan(db.Model):
-    __tablename__ = "fan"
+    __tablename__ = "fans"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
